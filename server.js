b5092d9f901e12d30b3d0e72f9da6bcb36a7b221
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-04-03 07:19:23 +0200

    Add server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'));
+});
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
